73838934de32c557f28239c7d688fe5593c191dd
taggeddev <denis.mikov.06@gmail.com>
2021-07-18 19:20:52 +0300

    The beginning of remeinder module

---
+++ Template/Modules/RemeinderModule.cs
@@ -0,0 +1,61 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Logging;
+using System.Timers;
+using System.Threading.Tasks;
+using System;
+using System.Threading;
+
+namespace Template.Modules
+{
+
+
+    public class RemeinderModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly ILogger<RemeinderModule> _logger;
+        private readonly DiscordSocketClient _client;
+
+        public RemeinderModule(ILogger<RemeinderModule> logger, DiscordSocketClient client)
+        {
+            _logger = logger;
+            _client = client;
+        }
+
+        [Command("enable_remeinder")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task EnableRemeinder()
+        {
+            double interval = 5 * 1000; // milliseconds to one hour
+
+            var nowTime = DateTime.Now.Minute;
+            var toWait = Math.Abs(nowTime - 32);
+
+            await ReplyAsync($"Таймер запуститься через {toWait} минут, не выключайте бота");
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                Thread.Sleep(toWait * 1000 * 60);
+
+                System.Timers.Timer checkForTime = new System.Timers.Timer(interval);
+                checkForTime.Elapsed += new ElapsedEventHandler(checkForTime_Elapsed);
+                checkForTime.Enabled = true;
+                SendRemeinderMessage();
+            }).Start();
+        }
+
+        private async void SendRemeinderMessage()
+        {
+            await _client.GetGuild(863151265939456043).GetTextChannel(863427166662557696).SendMessageAsync("Таймер запущен!");
+        }
+
+        private async void checkForTime_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            /*DateTime.Now.Hour == 18 && (DateTime.Now.DayOfWeek == DayOfWeek.Sunday || DateTime.Now.DayOfWeek == DayOfWeek.Wednesday)*/
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                await _client.GetGuild(863151265939456043).GetTextChannel(863427166662557696).SendMessageAsync("Вминаниме!");
+            }
+        }
+    }
+}
