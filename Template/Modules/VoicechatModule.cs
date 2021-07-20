a003484716de3ea3aa136936db5c8fb0e27f990d
taggeddev <denis.mikov.06@gmail.com>
2021-07-20 23:17:15 +0300

    feat: now commit_vc finds the user's vc and has a counts members there

--- Template/Modules/VoicechatModule.cs
+++ Template/Modules/VoicechatModule.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.Logging;
 using System;
@@ -8,19 +9,42 @@ using System.Threading.Tasks;
 
 namespace Template.Modules
 {
+    /// <summary>
+    /// Module with voicechat related commands
+    /// </summary>
     public class VoicechatModule : ModuleBase<SocketCommandContext>
     {
-        private readonly DiscordSocketClient _client;
-        public VoicechatModule(DiscordSocketClient client) => _client = client;
-
+        /// <summary>
+        /// VoiceChecker command is used to collect and mark all the users in the voice channel in the all-users-table
+        /// </summary>
         [Command("commit_vc")]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
         public async Task VoiceChecker()
         {
-            var vChannel = Context.Guild.GetVoiceChannel(866328191442223135);
-            var users = vChannel.Users;
-            await ReplyAsync($"{users.Count} участников зарегистрировано в канале {vChannel.Name}");
+            SocketVoiceChannel audioChannel = GetAuthorChannel();
+            
+            // If the executor is not in the voice channel
+            if (audioChannel == null)
+            {
+                await ReplyAsync("Вы не находитесь в канале.");
+                return;
+            }
+
+            var channelUsers = audioChannel.Users;
+            await ReplyAsync($"{channelUsers.Count} участников зарегистрировано в канале {audioChannel.Name}");
+
+            // Finds the voice channel where the command executor is currently in. If there is no VC with user - returns null
+            SocketVoiceChannel GetAuthorChannel()
+            {
+                foreach (SocketVoiceChannel voiceChannel in Context.Guild.VoiceChannels)
+                {
+                    IReadOnlyCollection<SocketGuildUser> users = voiceChannel.Users;
+                    foreach (var user in users)
+                        if (user.Id == Context.Message.Author.Id)
+                            return voiceChannel;
+                }
+                return null;
+            }
         }
-
-
     }
 }
