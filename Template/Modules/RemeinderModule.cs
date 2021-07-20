b066af33ed9b5838a2d6cb99ed4535ff385e50f2
taggeddev <denis.mikov.06@gmail.com>
2021-07-20 23:15:26 +0300

    refactor: methods are now local functions of the command

--- Template/Modules/RemeinderModule.cs
+++ Template/Modules/RemeinderModule.cs
@@ -9,44 +9,55 @@ using System.Threading;
 
 namespace Template.Modules
 {
+    /// <summary>
+    /// This module is used for Lessons reminder command
+    /// </summary>
     public class RemeinderModule : ModuleBase<SocketCommandContext>
     {
         private readonly DiscordSocketClient _client;
         public RemeinderModule(DiscordSocketClient client) => _client = client;
 
-        [Command("enable_remeinder")]
+        /// <summary>
+        /// Enable reminder command is used to turn on timer
+        /// </summary>
+        /// <returns></returns>
+        [Command("enable_reminder")]
         [RequireUserPermission(GuildPermission.Administrator)]
-        public async Task EnableRemeinder()
+        public async Task EnableReminder()
         {
-            double interval = 5 * 1000; // milliseconds to one hour
+            double interval = 5 * 1000; // milliseconds to 5 sec
 
             var nowTime = DateTime.Now.Minute;
-            var toWait = Math.Abs(nowTime - 32);
+            var toWait = Math.Abs(nowTime - 55);
 
             await ReplyAsync($"Таймер запуститься через {toWait} минут, не выключайте бота");
+
             new Thread(() =>
             {
                 Thread.CurrentThread.IsBackground = true;
-                Thread.Sleep(toWait * 1000 * 60);
+                Thread.Sleep(toWait * 1000 * 60); // Wait until its hh:55:ss and then turns on the checker
 
+                // Calls the CheckForTime every `interval` time
                 System.Timers.Timer checkForTime = new System.Timers.Timer(interval);
                 checkForTime.Elapsed += new ElapsedEventHandler(CheckForTime_Elapsed);
                 checkForTime.Enabled = true;
                 SendReminderMessage();
             }).Start();
-        }
 
-        private async void SendReminderMessage()
-        {
-            await _client.GetGuild(863151265939456043).GetTextChannel(863427166662557696).SendMessageAsync("Таймер запущен!");
-        }
-
-        private async void CheckForTime_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            /*DateTime.Now.Hour == 18 && (DateTime.Now.DayOfWeek == DayOfWeek.Sunday || DateTime.Now.DayOfWeek == DayOfWeek.Wednesday)*/
-            if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            // Sends message when timer is ready and is turned on
+            void SendReminderMessage()
             {
-                await _client.GetGuild(863151265939456043).GetTextChannel(863427166662557696).SendMessageAsync("Вминаниме!");
+                _client.GetGuild(863151265939456043).GetTextChannel(863427166662557696).SendMessageAsync("Таймер запущен!");
+            }
+
+            // Calls every `interval` time, checks if its the correct time and date to send a reminder message in #news channel
+            void CheckForTime_Elapsed(object sender, ElapsedEventArgs e)
+            {
+                // TODO:
+                // Add the time and date verification
+                if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+                    _client.GetGuild(863151265939456043).GetTextChannel(863427166662557696).SendMessageAsync("Вминаниме!");
+                
             }
         }
     }
