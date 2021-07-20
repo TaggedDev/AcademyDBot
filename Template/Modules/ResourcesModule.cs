64c0c6ff9257c2d1fc040f56e8be550a2cf5ae37
taggeddev <denis.mikov.06@gmail.com>
2021-07-20 23:15:57 +0300

    refactor: added command params null-checker and some docs

--- Template/Modules/ResourcesModule.cs
+++ Template/Modules/ResourcesModule.cs
@@ -17,34 +17,41 @@ namespace Template.Modules
         private readonly DiscordSocketClient _client;
         public ResourcesModule(DiscordSocketClient client) => _client = client;
 
-
+        /// <summary>
+        /// This command sends resource message suggestion to the admins chat
+        /// </summary>
+        /// <param name="url">resource URL</param>
+        /// <param name="description">resource Description</param>
         [Command("resource")]
         public async Task SendResource(string url = null, [Remainder]string description = null)
         {
-            if (url == null)
+            if (url == null || description == null)
             {
-                await ReplyAsync("Укажите ссылку на ресурс и описание");
-                return;
-            }
-            if (description == null)
-            {
-                await ReplyAsync("Укажите описание ресурса после ссылки");
+                await ReplyAsync("Неверный формат команды. Правильный формат: !resource `URL` `long description`. Всё поля обязательны");
                 return;
             }
 
             string text = $"**Новое предложение ресурса:** {url}\n**Описание:** {description}\n**От:** {Context.User.Mention}";
+            // I am not using Context.Guild because this command must be able to be executed in bot's DMs
+            // _client.GetGuild() works for both variants
             await _client.GetGuild(863151265939456043).GetTextChannel(863427166662557696).SendMessageAsync(text);
         }
 
+        /// <summary>
+        /// Command is used by admins and teachers to approve custom or their own resources in #resources channel
+        /// </summary>
+        /// <param name="url">resource URL</param>
+        /// <param name="desc">resource Description</param>
         [Command("apply_resource")]
         [RequireUserPermission(ChannelPermission.ManageMessages)]
         public async Task ApplyResource(string url = null, [Remainder] string desc = null)
         {
             if (url == null || desc == null)
             {
-                await ReplyAsync("Неверный формат команды. Правильный формат: !apply_resource url description. Всё поля обязательны");
+                await ReplyAsync("Неверный формат команды. Правильный формат: !apply_resource `URL` `long description`. Всё поля обязательны");
                 return;
             }
+
             var builder = new EmbedBuilder()
                 .WithTitle("Новый ресурс! :new:")
                 .WithDescription($"{desc}")
