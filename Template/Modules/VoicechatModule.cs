f178f87a5802789f3a32f80dd01faf409ced58cc
taggeddev <denis.mikov.06@gmail.com>
2021-07-20 12:42:41 +0300

    refactor: getting the users in the voice channel

--- Template/Modules/VoicechatModule.cs
+++ Template/Modules/VoicechatModule.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Template.Modules
 {
-    class VoicechatModule
+    public class VoicechatModule : ModuleBase<SocketCommandContext>
     {
+        private readonly ILogger<VoicechatModule> _logger;
+        private readonly DiscordSocketClient _client;
+
+        public VoicechatModule(ILogger<VoicechatModule> logger, DiscordSocketClient client)
+        {
+            _logger = logger;
+            _client = client;
+        }
+
+        [Command("commit_vc")]
+        public async Task VoiceChecker()
+        {
+            var vChannel = Context.Guild.GetVoiceChannel(866328191442223135);
+            var users = vChannel.Users;
+            await ReplyAsync($"{users.Count} участников зарегистрировано в канале {vChannel.Name}");
+        }
+
+
     }
 }
