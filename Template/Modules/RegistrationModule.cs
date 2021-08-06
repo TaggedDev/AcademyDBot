5af7f10f0e9b0159a07432753068bc726dfde612
taggeddev <denis.mikov.06@gmail.com>
2021-08-06 13:51:10 +0300

    feat: added new command

---
+++ Template/Modules/RegistrationModule.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template.Modules
+{
+    public class RegistrationModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("add_student")]
+        [Alias("student", "stud", "ыегвуте", "фвв_ыегвуте")]
+        public async Task AddDatabaseStudent(SocketGuildUser user)
+        {
+
+            await ReplyAsync($":white_check_mark: {user.Nickname} был добавлен в БД");
+        } 
+    }
+}
