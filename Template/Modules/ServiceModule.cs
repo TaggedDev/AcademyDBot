f17518fb600ea9ebad1a7bf1055e4f0efc4ea703
taggeddev <denis.mikov.06@gmail.com>
2021-09-25 18:48:16 +0300

    feat: debug version check command

---
+++ Template/Modules/ServiceModule.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Template.Services
+{
+    public class ServiceModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("version")]
+        [Alias("v")]
+        public async Task CheckVersion()
+        {
+            await ReplyAsync("Current version: " + SettingsHandler.Version);
+        }
+    }
+}
