365173020543d38d1467689218aab11999ecdaad
taggeddev <denis.mikov.06@gmail.com>
2021-10-06 18:35:31 +0300

    feat: voice chat commits extended

--- Template/Modules/VoicechatModule.cs
+++ Template/Modules/VoicechatModule.cs
@@ -4,6 +4,7 @@ using Discord.WebSocket;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,15 +29,31 @@ namespace Template.Modules
                 return;
             }
 
-            var today = DateTime.Now.Date.Date;
+            var today = DateTime.Now.Date;
             var usersInChannel = audioChannel.Users;
-            string message = $"Участники в канале {audioChannel.Name} [{today.Year}.{today.Month}.{today.Day} {today.Hour}:{today.Minute}]";
+            string message = $"Участники в канале `{audioChannel.Name}` [{today.Year}.{today.Month}.{today.Day} {today.Hour}:{today.Minute}]";
+            string tutorsMSG = $"\n<@&863158732170592297>\n", listenersMSG = "\n<@&863158815210078248>\n", orgsMSG = "\n<@&863158341094342696>\n";
+            int listeners = 0, tutors = 0, orgs = 0;
             foreach (SocketGuildUser user in usersInChannel)
             {
-                message += $"\n{user.Mention}";
+                if (user.Roles.Any(x => x.Id == 863158341094342696))
+                {
+                    orgsMSG += $"\n{user.Mention}";
+                    orgs++;
+                }
+                else if (user.Roles.Any(x => x.Id == 863158732170592297))
+                {
+                    tutorsMSG += $"\n{user.Mention}";
+                    tutors++;
+                }
+                else
+                {
+                    listenersMSG += $"\n{user.Mention}";
+                    listeners++;
+                }
             }
-            await ReplyAsync(message);
-            await ReplyAsync($"{usersInChannel.Count} участников зарегистрировано в канале {audioChannel.Name}");
+            await ReplyAsync(message + orgsMSG + tutorsMSG + listenersMSG);
+            await ReplyAsync($"{usersInChannel.Count} участников зарегистрировано в канале {audioChannel.Name}. {listeners}/{tutors}/{orgs} Слушателей/Преподавателей/Организаторов");
 
             // Finds the voice channel where the command executor is currently in. If there is no VC with user - returns null
             SocketVoiceChannel GetAuthorChannel()
